9cf6ee8040d3c95e0202fd7fa55af9e4b29805db
Daniel Lv <lgn21st@gmail.com>
2008-11-18 12:28:24 +0800

    add notification message if user's flash player version less than v9.0.1

--- lib/s3_swf_upload/view_helpers.rb
+++ lib/s3_swf_upload/view_helpers.rb
@@ -1,18 +1,24 @@
 module S3SwfUpload
   module ViewHelpers
     def s3_swf_upload_tag(options = {})
-      height        = options[:height] || 35
-      width         = options[:width] || 300
-      js_helper     = options[:js_helper].nil? ? true : options[:js_helper]
+      height    = options[:height] || 35
+      width     = options[:width]  || 300
+      js_helper = options[:js_helper].nil? ? true : options[:js_helper]
       
       out = ""
       out << %(
         <script type="text/javascript" src="/javascripts/swfobject.js"></script>
         <script type="text/javascript">
-          swfobject.embedSWF("/s3_upload.swf", "s3_upload", "300", "35", "9.0.124");
+          swfobject.embedSWF("/s3_upload.swf", "s3_upload", "#{width}", "#{height}", "9.0.0");
         </script>
       )
-      out << %(<div id="s3_upload"></div>)
+      
+      out << %(
+        <div id="s3_upload">
+          Please <a href="http://www.adobe.com/go/getflashplayer">Update</a> your Flash Player to Flash v9.0.1 or higher...
+        </div>
+      )
+      
       out << s3_swf_upload_init if js_helper
     end
     
